36c654d8de71ee5a55e5addb7725ae8a13d0a01e
Immortal <84240738+Immortal-youtube@users.noreply.github.com>
2026-06-17 04:16:28 +0530

    navbar fixed

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,9 +1,10 @@
 'use client';
+import { Menu } from "lucide-react";
 import { useState, useEffect} from "react";
 
 export default function Navbar() {
     const [viewportWidth, setViewportWidth] = useState<number | null>(null);
-
+    const [open,setOpen] = useState(true);
     useEffect(() => {
         // This code ONLY runs in the browser
         setViewportWidth(window.innerWidth);
@@ -26,11 +27,29 @@ export default function Navbar() {
         );
     }else{
         return (
-        <nav className="flex justify-center items-center gap-10 py-6 bg-linear-to-b from-green-800 via-green-950 to-transparent" >
-            {/* <a href="/" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">Home</a>
-            <a href="/about" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">About</a>
-            <a href="/myprojects" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">My Project</a>
-            <a href="/articles" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">Articles</a> */}
+        <nav className="justify-start items-center gap-10 py-6 bg-linear-to-b from-green-800 via-green-950 to-transparent" >
+            <button onClick={() => setOpen(!open)} className="pl-5"><Menu /></button>
+
+            <ul 
+                className={`flex flex-col items-center gap-6 overflow-hidden transition-all duration-500 ease-in-out ${
+                    open 
+                    ? "max-h-0 opacity-0 pointer-events-none" 
+                    : "max-h-64 opacity-100 text-center"
+                }`}
+                >
+                <li>
+                    <a href="/" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">Home</a>
+                </li>
+                <li>
+                    <a href="/about" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">About</a>
+                </li>
+                <li>
+                    <a href="/myprojects" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">My Project</a>
+                </li>
+                <li>
+                    <a href="/blogs" className="text-xl border-b border-b-transparent transition-all duration-300 ease-in-out hover:scale-125 hover:border-b-blue-500">Blogs</a>
+                </li>
+            </ul>
         </nav>
         );
     }
